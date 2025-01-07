32d36587dde9e366bd3fd32048e4c4d2b12a1280
fuxuras <furkancanokal1@gmail.com>
2025-01-07 13:57:54 +0300

    server async block solved

--- Server/server.py
+++ Server/server.py
@@ -1,11 +1,13 @@
+from concurrent.futures import ThreadPoolExecutor
 from numba import jit
 import threading
 from fastapi import FastAPI, HTTPException
 import uvicorn
+import asyncio
 
-load_value = 0
 
-class prime_finder(threading.Thread):
+
+class PrimeFinder(threading.Thread):
     def __init__(self, n):
         super().__init__()
         self.result = None
@@ -36,28 +38,38 @@ class prime_finder(threading.Thread):
 
 
 app = FastAPI()
+active_conn = 0
+load_value = 0
+thread_pool = ThreadPoolExecutor(max_workers=4)
+
+async def run_prime_finder(n):
+    prime_finder = PrimeFinder(n)
+    loop = asyncio.get_event_loop()
+    await loop.run_in_executor(thread_pool, prime_finder.run)
+    return prime_finder.result
 
 @app.get("/nth_prime")
 async def get_nth_prime(n: int):
-    global load_value
     if n <= 0:
         raise HTTPException(status_code=400, detail="Invalid input")
     try:
+        global active_conn
+        global load_value
         load_value += n
-        prime_finder_thread = prime_finder(n)
-        prime_finder_thread.start()
-        prime_finder_thread.join()
-        prime_number = prime_finder_thread.result
+        active_conn += 1
+        prime_number = await run_prime_finder(n)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     finally:
         load_value -= n
+        active_conn -= 1
     return {"nth_prime": prime_number}
 
-@app.get("/load_value")
-async def get_load_value():
+@app.get("/stats")
+async def get_active_conn():
+    global active_conn
     global load_value
-    return {"load_value": load_value}
+    return {"active_conn": active_conn, "load_value": load_value}
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
